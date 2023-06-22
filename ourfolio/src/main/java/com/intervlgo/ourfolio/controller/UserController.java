0304eab9b4b57d21b208f307d0ca300711e76263
unavailableusername09 <legeno09133@gmail.com>
2023-06-22 13:04:23 +0900

    bugfixed

--- ourfolio/src/main/java/com/intervlgo/ourfolio/controller/UserController.java
+++ ourfolio/src/main/java/com/intervlgo/ourfolio/controller/UserController.java
@@ -60,11 +60,6 @@ public class UserController {
         return userService.updateIdPassword(userIdPasswordDto, jwtToken);
     }
 
-    @PatchMapping("")
-    public ResponseEntity<UserDto> changeEmploymentStatus(@RequestHeader(name = "Authorization") String jwtToken) {
-        return userService.changeEmploymentStatus(jwtToken);
-    }
-
     @DeleteMapping("")
     public ResponseEntity<UserDto> deactivateAccount(@RequestHeader(name = "Authorization") String jwtToken) {
         return userService.deactivateAccount(jwtToken);
